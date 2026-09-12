2323b70faea83788595b029059a2dd973a2e91c9
renuka <pathlavathrenuka17@gmail.com>
2026-09-13 00:03:28 +0530

    Update Java Program

--- samplename.java
+++ samplename.java
@@ -3,5 +3,6 @@ public class Samplename {
         System.out.println("Hello Jenkins!");
         System.out.println("This is my Sample Java Program.");
         System.out.println("Jenkins CI/CD Pipeline is working successfully!");
+        System.out.println("Jenkins Automation Test");
     }
 }
