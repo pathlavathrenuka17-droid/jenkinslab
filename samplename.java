4fdb574d98ff53364cac5b3978f12e028a073552
renuka <pathlavathrenuka17@gmail.com>
2026-09-12 23:15:23 +0530

    samplename.java

---
+++ samplename.java
@@ -0,0 +1,7 @@
+public class Samplename {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+        System.out.println("This is my Sample Java Program.");
+        System.out.println("Jenkins CI/CD Pipeline is working successfully!");
+    }
+}
